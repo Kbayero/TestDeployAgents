2fc44cc7a6020c704c9b2d31357a6b95a2cd9725
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-01-24 19:09:27 +0200

    Add test2

--- agent/self/main.go
+++ agent/self/main.go
@@ -14,7 +14,7 @@ import (
 var h = holmes.New("debug", constants.SERV_NAME)
 
 func main() {
-	// Get current path test4
+	// Get current path test6
 	path, err := utils.GetMyPath()
 	if err != nil {
 		h.FatalError("Failed to get current path: %v", err)
