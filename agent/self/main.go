fd253cb2fcbf3e623e5489ef9593f45e4d873f35
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-01-24 17:52:52 +0200

    Test12

--- agent/self/main.go
+++ agent/self/main.go
@@ -14,7 +14,7 @@ import (
 var h = holmes.New("debug", constants.SERV_NAME)
 
 func main() {
-	// Get current path test11
+	// Get current path test12
 	path, err := utils.GetMyPath()
 	if err != nil {
 		h.FatalError("Failed to get current path: %v", err)
